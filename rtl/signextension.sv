f332eedb3b22298e2e9759f0b920a9d4794d299e
aa6dcc <142180482+aa6dcc@users.noreply.github.com>
2024-11-21 10:50:22 +0000

    Create signextension.sv

---
+++ rtl/signextension.sv
@@ -0,0 +1,18 @@
+module signextension #(,
+    parameter DATA_WIDTH = 32
+)(
+
+    input   logic [11:0]                ImmI,
+    input   logic                       ImmSrc,
+    output  logic [DATA_WIDTH-1:0]      ImmOp
+);
+
+//synchronous reset
+always_comb begin
+    case (Immsrc)
+        1'b1:ImmOp = {20{ImmI[11]}, ImmI};
+        default: ImmOp = 32'b0;
+    endcase
+end
+
+endmodule
