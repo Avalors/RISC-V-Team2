147fa93657381b63600898f4cb1b86dfd27065d6
3lson <139233586+3lson@users.noreply.github.com>
2024-11-28 12:21:26 +0000

    Removed doit.sh comments

---
+++ tb/obj_dir/registerfile_tb.d
@@ -0,0 +1,13 @@
+registerfile_tb.o: \
+  /Users/elsontho/Desktop/IAC_Labs/Final-RISCVT2/RISC-V-Team2/tb/test/registerfile_tb.cpp \
+  /Users/elsontho/Desktop/IAC_Labs/Final-RISCVT2/RISC-V-Team2/tb/test/sync_testbench.h \
+  /Users/elsontho/Desktop/IAC_Labs/Final-RISCVT2/RISC-V-Team2/tb/test/base_testbench.h \
+  Vdut.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilatedos.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated_config.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated_types.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated_funcs.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated_cov.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated_vcd_c.h \
+  /opt/homebrew/Cellar/verilator/5.030/share/verilator/include/verilated_trace.h
